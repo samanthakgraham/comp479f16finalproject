499733e71a0b0917b053c19a21fcdcad71a7b0d7
Samantha Graham <sa_grah@bathurst.encs.concordia.ca>
2016-11-17 15:35:59 -0500

    Finalizing indexer, having it calculate tf-idf and save it in a different index

--- indexer.py
+++ indexer.py
@@ -5,6 +5,13 @@ from bs4 import BeautifulSoup # Load BeautifulSoup, for parsing HTML
 import nltk # Load NLTK, for tokenizing
 import string # Import string for some useful things
 import os # Import the os module, for the os.walk function
+from math import log10 # We need log for idf
+
+# "raw" index in the form { 'term' : { docId : term frequency in docId } }, for the first pass
+rawIndex = {}
+
+# The new, final index will be of the form { 'term' : { docId: tf-idf weight for this term in this docId } }
+index = {}
 
 # Imports the list of stopwords from a file and saves them in a list for future use
 def importStopwords():
@@ -32,7 +39,7 @@ if __name__ == '__main__':
     # Instantiate the list of tokens
     tokenList = []
 
-    # Keep track of the docIds
+    # Keep track of the docIds; this is also the number of docs, N
     docId = 0
 
     # Walk thru the crawled pages directory
@@ -64,28 +71,52 @@ if __name__ == '__main__':
                     # Do a bunch of compression
                     if word not in string.punctuation and "//www.concordia.ca" not in word and len(word) != 0 and word not in stopWords and word != 'http' and word.isalpha():
                         # And put the word in our list of tokens
-                        tokenList.append([word.encode('ascii', 'ignore'), docId])
+                        tokenList.append([word.encode('ascii', 'ignore'), docId])	
 
-    # Now create the index
-    index = {}
+	# Save the number of documents in N to avoid confusion for later
+	N = docId
 
     # Go thru all the tokens in the list
-    for token, docId in tokenList:
+    for token, doc in tokenList:
         # If this token isn't in the index yet
-        if token not in index:
+        if token not in rawIndex:
             # Add it to the index
-            index[token] = {docId: 1}
+            rawIndex[token] = {doc: 1}
         else:
             # If this docId isn't in the postings list for this term
-            if docId not in index[token]:
+            if docId not in rawIndex[token]:
                 # Add it to the postings list
-                index[token].update({docId: 1})
+                rawIndex[token].update({doc: 1})
             else:
                 # Increment the term frequency for this term in this docId
-                index[token][docId] = index[token][docId] + 1
+                rawIndex[token][doc] = rawIndex[token][doc] + 1
+    
+	# Now, use the raw index to calculate tf-idf for each term in each document.	
 
-    print index
+	# Loop thru terms in rawIndex
+	for term in rawIndex:
+		# Save an entry for it in the final index
+		index[term] = {}
 
+		# Loop thru docIds for this term
+		for doc in rawIndex[term]:
+			# Get the term frequency, saved in the raw index
+			termFreq = rawIndex[term][doc]
+
+			# Get the document frequency, the length of the dict for this term
+			docFreq = len(rawIndex[term])
+
+			# Calculate the idf
+			inverseDocFreq = (log10(N)) / docFreq
+	
+			# Calculate tf-idf for this term in this document
+			tfIdf = termFreq * inverseDocFreq
+
+			# And save it in the index
+			index[term][doc] = tfIdf
+
+#print rawIndex
+print index
 
 
 
